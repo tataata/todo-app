171b5ef268a56df0861864d5f441a1bc8426b2a1
T <fortataata@gmail.com>
2023-10-23 19:44:48 +0200

    min styling for the form; add two ways of getting value of the input: with useState and useRef

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,15 +1,33 @@
-import { useState } from "react";
+import { useState, useRef, useId } from "react"
+import './AddItem.css'
 
 function AddItem( { addTask } ) {
-  const [ text, setText ] = useState('');
+  // Get the value of the input via useState
+  // -- when I want to provide instant validation feedback
+  // -- when I want to reset the value on submition
+  const [ text, setText ] = useState('')
+
+  // Get the value of the input via ref
+  // Ref is ok when I need a value only once, on submition
+  const textInputRef = useRef()
+
+  const todoItemId = useId()
+
   return (
     <form onSubmit={(event) => {
-      event.preventDefault();
-      addTask(text);
-      setText("");
+      event.preventDefault()
+      addTask(text)
+      setText("")
+      console.log(text + ' <-- grabbed with useState')
+      const enteredText = textInputRef.current.value
+      console.log(enteredText + ' <-- grabbed with useRef')
     }}>
+      <label htmlFor="{todoItemId}">What to do?</label>
       <input
-        placeholder="Add a todo"
+        id="{todoItemId}"
+        ref={textInputRef}
+        placeholder="write here"
+        pattern="^[a-zA-Z0-9 ]+$"
         type="text"
         value={text}
         onChange={(event) => {
