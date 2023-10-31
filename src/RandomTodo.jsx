fa19f1506a1ce5a2341a601007678e7628a56053
T <fortataata@gmail.com>
2023-10-31 08:37:52 +0100

    add error handling

--- src/RandomTodo.jsx
+++ src/RandomTodo.jsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from 'react'
 import './RandomTodo.css'
 
 function RandomTodo() {
+  // loading state to provide feedback on the screen while losding
+  const [isFetching, setIsFetching] = useState(false)
   // prepare the app for data fetching
-  const [randomTodo, setRandomTodo] = useState('...')
+  const [randomTodo, setRandomTodo] = useState('')
   const [error, setError] = useState('')
 
   // fetch return a promise
@@ -11,8 +13,7 @@ function RandomTodo() {
   // as it will be triggering the rerender ==> useEffect
   useEffect(() => {
     async function fetchRandomTodo() {
-     
-
+      setIsFetching(true)
       // handling HTTP errors
       //  if (response.ok) // 100, 200 -- all good
       // we are curious about 400, 500, when it is not okay
@@ -20,35 +21,49 @@ function RandomTodo() {
       // try {} catch {}
       // fetch can fail if htere is no connection so we def want to use try catch
       try {
-        // here is the code that might fail
-        const response = await fetch('https://www.boredapi.com/api/actrewivity')
-        const resData = await response.json()
+        const response = await fetch('https://www.boredapi.com/api/activity')
 
-        if (!response.ok) {
-          // const error = new Error('Failed to fetch random todos')
-          // throw error 
-          // or a shorter vesion of the 2 previoous lines
-          throw new Error('Failed to fetch random todos')
+        // if (!response.ok) {
+        //   throw new Error('Oh no! HTTP error status: ${response.status!');
+        // } else if (response.status === 404) {
+        //   throw new Error('Oh no! 404 -- page not found!');
+        // } else if (response.status === 500) {
+        //   throw new Error('Oh no! 500 -- server error!');
+        // }
+        if (response.status === 200) {
+          const resData = await response.json()
+          setRandomTodo(resData.activity)
+        } else {
+          setError('There was an error fetching the data')
+          throw new Error('...Oh no! Something went wrong :-(')
         }
-        setRandomTodo(resData.activity)
 
       } catch (error) {
-        // handling the errors here
-        // ususally handling an error means to update UI --> one more state
-        console.error('Failed to fetch random todos');
-        setError('Failed to fetch random todos')
+        // TypeError: Failed to fetch
+        console.error(error)
+        setError(error.message)
       }
+
+      setIsFetching(false) // not fetching anymore
     }
 
     // call the function in the scope of the useEffect
     fetchRandomTodo()
   }, [])
+
   
   return (
     <>
       <hr></hr>
-      <h2>Bored? Maybe <span className="text--lowercase">{randomTodo}</span>?</h2>
-      <p> {error ? error : ''} </p>
+      <h2>Bored? Maybe 
+        {isFetching && '...let us think...'} 
+        <span className="text--lowercase">
+        {error ? 
+          ' ' + error :
+          ' ' + randomTodo
+        }
+       </span> 
+      </h2> 
       {/* https://www.boredapi.com/api/activity */}
       {/* {"activity":"Repaint a room in your house",
            "type":"recreational",
