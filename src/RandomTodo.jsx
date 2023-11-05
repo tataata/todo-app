a8105513753b4ee7336b1be3f8eaed75efbbc628
T <fortataata@gmail.com>
2023-11-05 18:53:20 +0100

    cleanup eror handling

--- src/RandomTodo.jsx
+++ src/RandomTodo.jsx
@@ -2,76 +2,68 @@ import { useState, useEffect } from 'react'
 import './RandomTodo.css'
 
 function RandomTodo() {
-  // loading state to provide feedback on the screen while losding
+  // loading state tracking to provide feedback on the screen
   const [isFetching, setIsFetching] = useState(false)
   // prepare the app for data fetching
-  const [randomTodo, setRandomTodo] = useState('')
+  const [randomTodo, setRandomTodo] = useState([])
+  // state for errors messages
   const [error, setError] = useState('')
 
-  // fetch return a promise
-  // this will create the infinite loop
-  // as it will be triggering the rerender ==> useEffect
   useEffect(() => {
+    let ignore = false;
     async function fetchRandomTodo() {
-      setIsFetching(true)
-      // handling HTTP errors
-      //  if (response.ok) // 100, 200 -- all good
-      // we are curious about 400, 500, when it is not okay
-      // 
-      // try {} catch {}
-      // fetch can fail if htere is no connection so we def want to use try catch
+     
       try {
+        setIsFetching(true)
         const response = await fetch('https://www.boredapi.com/api/activity')
 
-        // if (!response.ok) {
-        //   throw new Error('Oh no! HTTP error status: ${response.status!');
-        // } else if (response.status === 404) {
-        //   throw new Error('Oh no! 404 -- page not found!');
-        // } else if (response.status === 500) {
-        //   throw new Error('Oh no! 500 -- server error!');
-        // }
-        if (response.status === 200) {
-          const resData = await response.json()
-          setRandomTodo(resData.activity)
+        if(!response.ok) {
+          setError('...Oh no! Something went wrong. Come back later :-(')
+          throw new Error('Error occurred with HTTP connection')
         } else {
-          setError('There was an error fetching the data')
-          throw new Error('...Oh no! Something went wrong :-(')
+          console.log('i ma here')
+          const resData = await response.json()
+          if (!ignore) {
+            setRandomTodo(resData.activity)
+          }
         }
 
-      } catch (error) {
-        // TypeError: Failed to fetch
+      } catch (error) { 
+        // Display the technical error message in the console
         console.error(error)
-        setError(error.message)
+        // display more human language error message for the user in the browser window
+        setError('...oh no! It seems that the error occured :-( Come back later')
       }
 
-      setIsFetching(false) // not fetching anymore
+      setIsFetching(false)
     }
-
-    // call the function in the scope of the useEffect
     fetchRandomTodo()
+    // cleanup   
+    return () => { ignore = true }
   }, [])
 
-  
   return (
     <>
       <hr></hr>
-      <h2>Bored? Maybe 
-        {isFetching && '...let us think...'} 
-        <span className="text--lowercase">
-        {error ? 
-          ' ' + error :
-          ' ' + randomTodo
-        }
-       </span> 
-      </h2> 
-      {/* https://www.boredapi.com/api/activity */}
-      {/* {"activity":"Repaint a room in your house",
-           "type":"recreational",
-           "participants":1,
-           "price":0.3,
-           "link":"",
-           "key":"4877086",
-           "accessibility":0.4} */}
+      <h2>Bored? 
+        Maybe 
+        {isFetching && '...let us think...'}
+        <span className="text--lowcase">
+          {error 
+            ? ' ' + error
+            : ' ' + randomTodo
+          }
+        </span></h2>
+      {/* 
+      https://www.boredapi.com/api/activity 
+      :
+      {"activity":"Match your storage containers with their lids",
+      "type":"busywork",
+      "participants":1,
+      "price":0,
+      "link":"",
+      "key":"4940907",
+      "accessibility":1} */}
     </>
   )
 }
